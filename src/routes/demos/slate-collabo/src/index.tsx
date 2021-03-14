2ab1296fffa6bae35459aae9dc8c55675259e6d2
buhi <yinshang1992@gmail.com>
2021-03-14 22:40:46 +0800

    update

--- src/routes/demos/slate-collabo/src/index.tsx
+++ src/routes/demos/slate-collabo/src/index.tsx
@@ -1,6 +1,16 @@
 import App from "./editor"
-import React from "react"
+import React, { useState } from "react"
 
 export default function SlatejsCollabo() {
-    return <App />
+
+    const [open, setOpen] = useState(false)
+
+    return <div>
+            {open && <App /> || <>
+            <div>要看demo, 得先点击左上小锁图标>网站设置>不安全内容>允许, 因为我没有证书的域名, 不然就跑不起来的.</div>
+            <button onClick={()=>{
+                setOpen(true)
+            }}>设置好了</button>
+        </>}
+    </div>
 }
